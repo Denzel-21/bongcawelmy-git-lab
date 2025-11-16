ad0e7c7582268b74f360938d5d954f23a947db03
Denzel-21 <denzelpaulbongcawel2002@gmail.com>
2025-11-16 22:56:09 +0800

    Update feature1.py with working feature_one function

--- feature1.py
+++ feature1.py
@@ -1,1 +1,6 @@
-"print('This is Feature 1 script')" 
+print("This is feature 1 python script")
+
+def feature_one():
+    print("Feature 1 is working! This feature was created in the feature-1 branch.")
+
+feature_one()
